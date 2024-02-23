066b034a345c5e5126efa23a3bf59a76c8158f53
Jake Foster <42629538+JgFoster12@users.noreply.github.com>
2024-02-23 10:01:20 -0500

    Update passwordgen.py
    Update the logic to ensure that the password is at least 16 characters.

--- passwordgen.py
+++ passwordgen.py
@@ -4,14 +4,14 @@ import random
 wordList = open("wordlist2.txt").read()
 wordArr = wordList.splitlines()
 password = " "
+i = 0
 
-for i in range(3):
-   r= random.randint(0, 438)
-   index = r
-   if(i==0):
+while(len(password) < 16 and i < 3):
+  r= random.randint(0, 438)
+  index = r
+  if(i==0):
     password += str(wordArr[index])
 
-   else: 
-     password += "-"+str(wordArr[index])
-
-print("Your temporary password is " + password)
+  else: 
+    password += "-"+str(wordArr[index])
+  i+=1
